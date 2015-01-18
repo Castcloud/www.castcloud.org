83e531fffd357c79c26f883d669784f93aa1d643
khlieng <kk_998@hotmail.com>
2015-01-18 21:52:36 +0100

    Added package.json, fixed the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function() {
-    gulp.src('./*.css')
+gulp.task('css', function() {
+    gulp.src('./src/*.css')
         .pipe(autoprefixer())
         .pipe(gulp.dest('./'));
 });
+
+gulp.task('watch', ['default'], function() {
+    gulp.watch('./src/*.css', ['css']);
+});
+
+gulp.task('default', ['css']);
